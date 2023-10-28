2402608178960ab562d86d74a005efdbeffe7c50
Aishkg5 <149239187+Aishkg5@users.noreply.github.com>
2023-10-28 16:21:13 +0530

    Update streamlit_app.py
    added emojis for the items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & blueberry oatmeal')
-streamlit.text('Kale,spinach and rocket smoothie')
-streamlit.text('Hard boiled free-range egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & blueberry oatmeal')
+streamlit.text('🥗 Kale,spinach and rocket smoothie')
+streamlit.text('🐔 Hard boiled free-range egg')
+streamlit.text('🥑🍞 Avacado toast')
